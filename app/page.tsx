9fac7483361cb0c3d8ae949965e4f6b1ec1df1ff
pablojohn martinez <pablojohn.martinez@gmail.com>
2024-12-27 08:55:23 -0600

    wip: swithing to next-auth beta

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div className="">
-      Home Component
+    <div className="flex flex-col gap-6">
+      <h1 className="text-3xl font-bold">WoW Behave</h1>
+      <div>
+        This is a site used to rate m+ teammates after playing with them.
+      </div>
     </div>
   );
 }
