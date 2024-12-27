4feea3be7eed73c0b8754ce91421c7911743fc5d
pablojohn martinez <pablojohn.martinez@gmail.com>
2024-12-27 13:14:46 -0600

    wip: added signout button - now going to clean up styling a little

---
+++ app/components/signout-button.tsx
@@ -0,0 +1,16 @@
+import { signOut } from "auth";
+import { Button } from "./button"
+
+export default function SignOutButton(props: React.ComponentPropsWithRef<typeof Button>) {
+  return (
+    <form action={async () => {
+      "use server"
+      await signOut()
+    }}
+      className="w-full">
+      <Button variant="ghost" className="w-full p-0" {...props}>
+        Signout
+      </Button>
+    </form>
+  )
+}
--- app/components/user-button.tsx
+++ app/components/user-button.tsx
@@ -1,5 +1,15 @@
 import { auth } from "auth"
-import SignInButton  from "./signin-button"
+import { Avatar, AvatarImage } from "./avatar";
+import { Button } from "./button";
+import SignInButton from "./signin-button";
+import SignOutButton from "./signout-button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 
 export default async function UserButton() {
     const session = await auth();
@@ -9,7 +19,36 @@ export default async function UserButton() {
             <span className="hidden text-sm sm:inline-flex">
                 {session.user.email}
             </span>
-            <p>{session.user.name}</p>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+                        <Avatar className="h-8 w-8">
+                            <AvatarImage
+                                src={
+                                    session.user.image ??
+                                    `https://api.dicebear.com/9.x/thumbs/svg?seed=${Math.floor(Math.random() * 100000) + 1}&randomizeIds=true`
+                                }
+                                alt={session.user.name ?? ""}
+                            />
+                        </Avatar>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="end" forceMount>
+                    <DropdownMenuLabel className="font-normal">
+                        <div className="flex flex-col space-y-1">
+                            <p className="text-sm font-medium leading-none">
+                                {session.user.name}
+                            </p>
+                            <p className="text-muted-foreground text-xs leading-none">
+                                {session.user.email}
+                            </p>
+                        </div>
+                    </DropdownMenuLabel>
+                    <DropdownMenuItem>
+                        <SignOutButton />
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     )
 }
