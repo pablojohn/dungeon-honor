aad35ff7c2336868fe2cdb394da5b52a1d49729f
pablojohn martinez <pablojohn.martinez@gmail.com>
2024-12-29 14:33:15 -0600

    wip: change coffee icon in nav to a home icon that links back to home page

--- app/components/main-nav.tsx
+++ app/components/main-nav.tsx
@@ -1,9 +1,12 @@
-import { Coffee } from "lucide-react"
+import Link from "next/link";
+import { House } from "lucide-react";
 
 export default function MainNav() {
     return (
         <div className="flex items-center gap-4">
-            <Coffee />
+            <Link href="/" className="flex items-center p-2 rounded hover:bg-gray-200 transition">
+                <House />
+            </Link>
         </div>
-    )
+    );
 }
