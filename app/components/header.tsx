bdbbb1a5f1fce90596f88866b4ac35463e162c63
pablojohn martinez <pablojohn.martinez@gmail.com>
2024-12-29 20:40:33 -0600

    wip: make the header sticky

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@ import UserButton from "./user-button";
 
 export default function Header() {
     return (
-        <header className="sticky flex justify-center border-b">
+        <header className="sticky top-0 z-50 flex justify-center border-b bg-white">
             <div className="mx-auto flex h-16 w-full max-w-3xl items-center justify-between px-4 sm:px-6">
                 <MainNav />
                 <UserButton />
