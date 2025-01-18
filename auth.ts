3fe93578598b7bb97b26a59b13ac40c21c8780a3
pablojohn martinez <pablojohn.martinez@gmail.com>
2025-01-18 14:16:14 -0600

    bugfix: adding pkce and nonce checks to fix auth error

--- auth.ts
+++ auth.ts
@@ -21,6 +21,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   debug: true,
   providers: [
     BattleNet({
+      checks: ['pkce', 'nonce'],
       clientId: process.env.BATTLENET_CLIENT_ID,
       clientSecret: process.env.BATTLENET_CLIENT_SECRET,
       issuer: issuer,
